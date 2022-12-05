49d9f27d94c024e3c0f4e61f2db7371f6cb0bde8
Aleno <aleno.jankowsky@5minds.de>
2022-12-05 14:53:57 +0100

    Add solution for win_validation

--- StatusValidator.py
+++ StatusValidator.py
@@ -3,19 +3,17 @@ class StatusValidator:
         pass
 
     def is_win(self, board, player_symbol) -> bool:
-        if self.is_horizontal_win(board, player_symbol) or self.is_vertical_win(board, player_symbol):
-            return True
+        return self.is_horizontal_win(board, player_symbol) or self.is_vertical_win(board, player_symbol) or self.is_diagonal_win(board, player_symbol)
 
     def is_horizontal_win(self, board, player_symbol) -> bool:
-        sequence_counter = 0
         for row in board.board:
+            sequence_counter = 0
             for cell in row:
                 if sequence_counter == 4:
                     return True
 
                 if player_symbol in cell:
                     sequence_counter += 1
-                    continue
                 else: 
                     sequence_counter = 0
 
@@ -23,47 +21,45 @@ class StatusValidator:
 
     def is_vertical_win(self, board, player_symbol) -> bool:
         sequence_counter = 0
-        cell_index_in_row = 0
-
-        for row in board.board:
-            if sequence_counter == 4:
-                return True
-            
-            if player_symbol in row[cell_index_in_row]:
-                sequence_counter += 1
-                continue
-            else:
-                sequence_counter = 0
-
-
+        for cell_index_in_row in range(5):
+            for row in board.board:
+                if sequence_counter == 4:
+                    return True
+                
+                if player_symbol in row[cell_index_in_row]:
+                    sequence_counter += 1
+                else:
+                    sequence_counter = 0
+                    
         return False
 
     def is_diagonal_win(self, board, player_symbol) -> bool:
         sequence_counter = 0
-        cell_index_in_row_from_left = 0
-        cell_index_in_row_from_right = 5
-        for row in board.board:
-            if sequence_counter == 4:
-                return True
-            
-            if player_symbol in row[cell_index_in_row_from_left]:
-                sequence_counter += 1
-                cell_index_in_row_from_left += 1
-                print(sequence_counter)
-                continue
-            else:
-                sequence_counter = 0
+        for cell_index_in_row in range(2):
+            for row in board.board:
+                old_cell_index_in_row = cell_index_in_row
+                if sequence_counter == 4:
+                    return True
+                
+                player_symbol_is_in_current_cell = player_symbol in row[cell_index_in_row]
+                if player_symbol_is_in_current_cell:
+                    sequence_counter += 1
+                    cell_index_in_row += 1
+                else:
+                    sequence_counter = 0
+                    cell_index_in_row = old_cell_index_in_row
 
-        for row in board.board:
-            if sequence_counter == 4:
-                return True
+        sequence_counter = 0
+        for cell_index_in_row in range(5, 3, -1):
+            for row in board.board:
+                old_cell_index_in_row = cell_index_in_row
+                if sequence_counter == 4:
+                    return True
 
-            if player_symbol in row[cell_index_in_row_from_right]:
-                sequence_counter += 1
-                cell_index_in_row_from_right -= 1
-                continue
-            else:
-                sequence_counter = 0
-
-        return False
-        
+                player_symbol_is_in_current_cell = player_symbol in row[cell_index_in_row]
+                if player_symbol_is_in_current_cell:
+                    sequence_counter += 1
+                    cell_index_in_row -= 1
+                else:
+                    sequence_counter = 0
+                    cell_index_in_row = old_cell_index_in_row
